ada266c77ee8167944feea9e6c867be39b108286
dmevola <dmevola@gmail.com>
2022-01-16 15:35:51 -0500

    added card for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
-var city = "Detroit"
+var city = "";
 
-var getWeather = function() {
-    var queryURL = 
-    "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=35de209ea2cd44495c1feb05459c71ed&q=" + city;
+function getWeather() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=35de209ea2cd44495c1feb05459c71ed&q=" + city;
     fetch(queryURL).then(function(response) {
     if (response.ok)
     response.json().then(function(data) {
     console.log(data)
     }
     )}
-    )}
+)};
+
+function searchFunction() {
+    event.preventDefault();
+    city = $("#city-input").val();
+    getWeather();
+}
+
+//event listener
+$("#search-button").on("click", searchFunction)
 
 
-
-getWeather()
